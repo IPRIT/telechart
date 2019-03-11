b3ded28e49111e6f43f3a29c8afa6506ffeb19e6
IPRIT <ipritoflex@yandex.ru>
2019-03-11 20:04:41 +0300

    Update

--- webpack.dev.js
+++ webpack.dev.js
@@ -49,8 +49,7 @@ module.exports = {
                         }
                     }
                 ]
-            }
-            ,
+            },
             {
                 // Load all images as base64 encoding if they are smaller than 8192 bytes
                 test: /\.(png|jpg|gif)$/,
